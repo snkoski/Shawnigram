80400dc1ab9dae289eecaa65502c741c6116b08e
Shawn Koski <skoski@artnet.com>
2021-04-30 18:59:40 -0400

    Create routes constants. Create initial login page. Add react-router-dom to App.js.

---
+++ src/pages/login.js
@@ -0,0 +1,26 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Shawnigram';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <p>I am trying to work</p>
+    </div>
+  );
+}
