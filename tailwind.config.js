72b7f85bbbe13be3ec64fda1453ee9456e5def8c
Shawn Koski <skoski@artnet.com>
2021-05-01 11:25:26 -0400

    Add color values to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,28 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true
+  },
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#005c98',
+        faded: '#000059'
+      },
+      red: {
+        primary: '#ed4956'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      }
+    }
   }
 };
