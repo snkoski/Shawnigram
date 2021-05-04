e7df56156a7d4f841db5403b6ef5359ef5ef8910
Shawn Koski <skoski@artnet.com>
2021-05-04 19:23:17 -0400

    whyDidYouRender file. whyDidYouRender added to index.js. whyDidYouRender added to sidebar/index.js.

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,16 +1,20 @@
+/* eslint-disable no-plusplus */
+import { useState } from 'react';
 import UseUser from '../../hooks/use-user';
 import User from './user';
 import Suggestions from './suggestions';
 
 export default function Sidebar() {
   const {
-    user: { fullName, userName, userId }
+    user: { fullName, username, userId, following }
   } = UseUser();
 
   return (
     <div className="p-4">
-      <User userName={userName} fullName={fullName} />
-      <Suggestions userId={userId} />
+      <User username={username} fullName={fullName} />
+      <Suggestions userId={userId} following={following} />
     </div>
   );
 }
+
+Sidebar.whyDidYouRender = true;
